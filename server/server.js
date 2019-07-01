62d21601f946180e3f1d78daf344254a14d29fc0
Danielle Martain <dmartain@gmail.com>
2019-07-01 11:49:57 -0500

    adding heroku

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use('/movies', movieRouter);
 
 // Start the server listening
 // do this last, after setting up routes, and all the things
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen( PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 })
